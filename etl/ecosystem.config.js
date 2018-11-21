49b66351aa74483ff1c11a2de326c7e0dee3ef7e
Jules Dourlens <jdourlens@gmail.com>
2018-11-21 21:00:36 +0400

    updated startup script

--- etl/ecosystem.config.js
+++ etl/ecosystem.config.js
@@ -1,30 +1,15 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
+    name: 'live',
+    script: 'live.js',
     autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+    watch       : true,
+  },
+  {
+    name: 'past',
+    script: 'past.js',
+    autorestart: true,
+    watch       : true,
   }],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
