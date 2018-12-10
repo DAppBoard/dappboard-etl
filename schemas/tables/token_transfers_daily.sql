d04d2c02949817c62298c0a6163d464936160940
Jules Dourlens <jdourlens@gmail.com>
2018-12-10 20:12:52 +0400

    Token summary table and daily

---
+++ schemas/tables/token_transfers_daily.sql
@@ -0,0 +1,17 @@
+CREATE MATERIALIZED VIEW token_transfers_daily AS
+SELECT date_trunc('day', "timestamp") "day",
+ token_address,
+ COUNT(*) AS transfers,
+ COUNT(DISTINCT(from_address)) AS senders,
+ COUNT(DISTINCT(to_address)) AS receivers,
+ COUNT(DISTINCT(value)) AS nft_distincts,
+ SUM(value) AS erc20_volume,
+ AVG(value) AS erc20_average,
+ MAX(value) AS erc20_maximum
+ FROM token_transfers
+ WHERE "timestamp" > NOW() - INTERVAL '7 day'
+ GROUP BY day, token_address ORDER BY "day" DESC  WITH NO DATA;
+
+ CREATE INDEX token_transfers_daily_day ON token_transfers_daily (day);
+
+ REFRESH MATERIALIZED VIEW token_transfers_daily ;
