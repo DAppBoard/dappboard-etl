da3f6be8945b826061c5821c84ceebb13eafdf7d
Jules Dourlens <jdourlens@gmail.com>
2019-01-24 12:47:04 +0400

    Add 0x daily view

---
+++ schemas/views/009_dapp_0x_daily.sql
@@ -0,0 +1,8 @@
+CREATE VIEW dapp_0x_daily AS
+SELECT CAST("public"."dapp_0x"."timestamp" AS date) AS "date",
+  count(*) AS "trades",
+ count(distinct( "public"."dapp_0x"."maker_token", "public"."dapp_0x"."taker_token")) AS "pairs",
+ count(distinct "public"."dapp_0x"."relayer") AS "relayers"
+FROM "public"."dapp_0x"
+GROUP BY CAST("public"."dapp_0x"."timestamp" AS date)
+ORDER BY CAST("public"."dapp_0x"."timestamp" AS date) ASC
--- schemas/views/009_dapp_0x_pairs.sql
+++ schemas/views/009_dapp_0x_pairs.sql
@@ -1,6 +1,6 @@
 CREATE VIEW dapp_0x_pairs AS
 SELECT
-COUNT(*) as count_of_trades, ftoken.name as fname, stoken.name as sname,  ftoken.symbol as fsym, stoken.symbol as ssym, ftoken.address as faddress, stoken.address as saddress, COUNT(DISTINCT(maker, taker)) AS count_of_traders
+COUNT(*) as count_of_trades, COUNT(DISTINCT(relayer)) AS count_of_relayers, ftoken.name as fname, stoken.name as sname,  ftoken.symbol as fsym, stoken.symbol as ssym, ftoken.address as faddress, stoken.address as saddress, COUNT(DISTINCT(maker, taker)) AS count_of_traders
  FROM dapp_0x
      LEFT JOIN tokens ftoken ON GREATEST(maker_token, taker_token)=ftoken.address
      LEFT JOIN tokens stoken ON LEAST(maker_token, taker_token)=stoken.address
